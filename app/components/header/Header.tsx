8e9593e78ca09b9d3e59b0f194c4a6dcb29ab223
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2024-07-26 09:52:47 -0300

    Colors selected for header and background

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,11 +1,14 @@
+import { Search } from "@mui/icons-material"
+
 export default function Header() {
   return (
-    <header className="w-full h-24 border-b flex items-center justify-between">
-      <div className="ml-5 w-2/6">
-        <input type="text" className="w-auto p-2 h-10 border-black border shadow-sm rounded-md" placeholder="Pesquise o heroi" />
-      </div>
-      <div className="self-center w-full flex justify-center">
-        <span>Desafio estagio Logisitca LTDA</span>
+    <header className="w-full h-24 flex items-center bg-[#f7f7f7]">
+      <div className="ml-5 relative flex items-center">
+        <input type="text"
+          className="w-full p-3 h-10 shadow-sm rounded-md focus:outline-none"
+          placeholder={`Pesquise o heroi`}
+        />
+        <Search className="absolute right-1 opacity-20"/>
       </div>
     </header>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from "./components/header/Header";
 
 export default function Home() {
   return (
-    <div className="max-w-screen max-h-screen">
+    <div className="max-w-screen max-h-screen bg-slate-800">
       <section className="w-screen h-screen">
         <Header />
       </section>
